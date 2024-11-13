c74f68d7059558f41f0648e06b58ee70fafa6f63
Noa Aarts <noa@voorwaarts.nl>
2024-11-13 17:28:58 +0100

    add test for color

--- src/color.rs
+++ src/color.rs
@@ -37,3 +37,27 @@ impl Display for Color {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(Color::Black, "black")]
+    #[case(Color::Red, "red")]
+    #[case(Color::Green, "green")]
+    #[case(Color::Yellow, "yellow")]
+    #[case(Color::Blue, "blue")]
+    #[case(Color::Magenta, "magenta")]
+    #[case(Color::Cyan, "cyan")]
+    #[case(Color::White, "white")]
+    #[case(Color::Byte(28), "#008700")]
+    #[case(Color::Byte(147), "#AFAFFF")]
+    #[case(Color::Byte(249), "#B2B2B2")]
+    #[case(Color::Byte(172), "#D78700")]
+    #[case(Color::Full(0x42, 0x69, 0xAD), "#4269AD")]
+    fn color_parsing_returns_correct(#[case] color: Color, #[case] correct: &str) {
+        assert_eq!(color.to_string(), correct);
+    }
+}
